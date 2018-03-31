a2047061831c3305d10c8f77b1a8fd0500f10f40
Paul Fauchon <p.fauchon@gmail.com>
2018-03-31 08:28:44 +0800

    fix build script output

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo "Building unifi-throughput $VERSION  os:$OS arch:$ARCH"
 
 
 
-go build -ldflags="-s -w -X main.VERSION=$VERSION" unifi-throughput
+go build -ldflags="-s -w -X main.VERSION=$VERSION" -o unifi-throughput
 #Compressing
 
 tar -czf unifi-throughput-$VERSION-$OS-$ARCH.tar.gz unifi-throughput
