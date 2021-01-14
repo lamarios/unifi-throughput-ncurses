667449c39eea4c5da61a6db777b0d9b4e37d9494
Paul Fauchon <p.fauchon@gmail.com>
2021-01-14 11:53:30 +0800

    upload script for releases

--- upload_release.sh
+++ upload_release.sh
@@ -2,4 +2,4 @@ export RELEASE=$(curl -s "https://api.github.com/repos/lamarios/unifi-throughput
 export FILE="$(ls unifi-throughput-*.tar.gz)"
 export FILE_PATH="$(pwd)/$FILE"
 echo "Release $RELEASE, File $FILE - $FILE_PATH"
-curl --location --request POST "https://uploads.github.com/repos/lamarios/unifi-throughput-ncurses/releases/${RELEASE}/assets?access_token=${GITHUB_TOKEN}&name=${FILE}" --data-binary "@${FILE_PATH}" -H "Content-type: application/tar+gzip"
+curl --location --request POST "https://uploads.github.com/repos/lamarios/unifi-throughput-ncurses/releases/${RELEASE}/assets?name=${FILE}" --data-binary "@${FILE_PATH}" -H "Content-type: application/tar+gzip" -H "Authorization: token ${GITHUB_TOKEN}"
